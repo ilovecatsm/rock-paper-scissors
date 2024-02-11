834d2c9ca16a7cc3cdca2cf4b1998aef37520308
Kendall Taliaferro <153865120+ilovecatsm@users.noreply.github.com>
2024-02-10 23:12:45 -0600

    for some reason my code keeps throwing me the same text. Ive spent so much time trying to figure out what's wrong T__T going to figure it out

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 // Create the variables for the rock, paper, scissors done
-// make the function (this function will be for letting the computer pick what it wants)
+// make the function (this function will be for letting the computer pick what it wants) done
 // let the user choose what they want to pick
 // the computer randomly chooses what it'll pick based of the variables from earlier
 // win or lose scenario output
 
 let choices = ["Rock", "Paper", "Scissors"];
 
+// gets player input
+function playerSelection() {
+  prompt("What will you choose?");
+  if (playerSelection === "Rock") {
+    return "You chose Rock";
+  } else if (playerSelection === "Paper") {
+    return "You chose Paper.";
+  } else if (playerSelection === "Scissors") {
+    return "You chose Scissors.";
+  } else return "Please type rock, paper, or scissors!";
+}
+// get player input
+// set the player input to show as all lowercase except for the first letter (which should always be zero)
+// console.log it out
+
 // computer choices
 function getComputerChoice() {
-  let computerChoice = Math.floor(Math.random() * 3 + 1);
+  let computerChoice = Math.floor(Math.random() * choices.length);
   if (computerChoice === 1) {
     return "The computer chose Rock.";
   } else if (computerChoice === 2) {
