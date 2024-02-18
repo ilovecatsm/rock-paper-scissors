374ce63ce8208a073e5d4b10224b5103f47a2b6b
Kendall Taliaferro <153865120+ilovecatsm@users.noreply.github.com>
2024-02-17 19:51:22 -0600

    figuring out how to set up the playerInput in a way that works

--- script.js
+++ script.js
@@ -1,69 +1,45 @@
 const choices = ["Rock", "Paper", "Scissors"];
-const playerSelection = userInput();
+const playerSelection = playerInput();
 const computerSelection = getComputerChoice();
-// gets player input and checks it
-function userInput() {
-  let playerInput = prompt("Choose Rock, Paper, or Scissors").toLowerCase();
-  if (playerInput === "rock") {
-    return "Rock";
-  } else if (playerInput === "paper") {
-    return "Paper";
-  } else if (playerInput === "scissors") {
-    return "Scissors";
-  } else "Please only type Rock, Paper, or Scissors!";
-  return playerInput;
-}
-function checkPlayerChoice(playerChoice) {
-  if (playerChoice === "rock") {
-    return "You selected Rock";
-  } else if (playerChoice === "paper") {
-    return "You selected Paper";
-  } else return "You selected Scissors";
-}
+
 // computer choices
 function getComputerChoice() {
-  const computerChoice = Math.floor(Math.random() * choices.length);
-  if (computerChoice === 1) {
-    return "Rock";
-  } else if (computerChoice === 2) {
-    return "Paper";
-  } else return "Scissors";
+  const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+  return computerChoice;
 }
-function checkComputerChoice(computerChoice) {
-  if (computerChoice === 1) {
-    return "The computer selected Rock";
-  } else if (computerChoice === 2) {
-    return "The computer selected Paper";
-  } else return "The computer selected Scissors";
-}
-// checks who wins the game
-function checkWinner(playerChoice, computerChoice) {
-  if (playerChoice === computerChoice) {
+// checks who won the round
+function checkWinner(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
     return "Tie";
   } else if (
-    (playerChoice === "Paper" && computerChoice === "Rock") ||
-    (playerChoice === "Scissors" && computerChoice === "Paper") ||
-    (playerChoice === "Rock" && computerChoice === "Scissors")
-  )
+    (playerSelection === "Rock" && computerSelection === "Scissors") ||
+    (playerSelection === "Scissors" && computerSelection === "Paper") ||
+    (playerSelection === "Paper" && computerSelection === "Rock")
+  ) {
     return "Player";
-  else {
-    return "Computer";
-  }
+  } else return "Computer";
 }
-// initializes the game
+// initializes the round
 function playRound(playerSelection, computerSelection) {
-  // console.log(userInput());
-  // console.log(getComputerChoice());
-
   const winner = checkWinner(playerSelection, computerSelection);
-  if (winner == "Tie") {
-    return "It's a tie!";
-  } else if (winner == "Player") {
+  if (winner === "Tie") {
+    return `It's a tie! You both chose ${playerSelection}`;
+  } else if (winner === "Player") {
     return `You win! ${playerSelection} beats ${computerSelection}`;
-  } else {
-    return `You lose! ${computerSelection} beats ${playerSelection}`;
-  }
+  } else return `You lose! ${computerSelection} beats ${playerSelection}`;
 }
-console.log(checkPlayerChoice());
-console.log(checkComputerChoice());
-console.log(playRound(playerSelection, computerSelection));
+// gets player input and checks it
+function playerInput() {
+  
+}
+// initializes the game in general (including the score)
+function playGame() {
+  console.log("Game Starting");
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = playerInput();
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+  }
+  console.log("Play again?");
+}
+console.log(playGame());
